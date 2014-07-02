7d622766c2c1f9c367d115a5fe944e7dfc2d018f
jecnua <jecnua@gmail.com>
2014-07-02 19:46:38 +0100

    Implemented anagarm exercise. Working but horrible.

--- main.php
+++ main.php
@@ -6,7 +6,6 @@
  */
 
 function invert_string($invertMe){
-
     if (!is_string($invertMe)) {
         throw new \Exception('You need to pass a string');
     }
@@ -20,6 +19,30 @@ function invert_string($invertMe){
     return implode('',$invertedArray);
 }
 
-
+function is_anagram($originalString, $anagramCandidate){
+    $originalStringAsArray = str_split($originalString);
+    $dataSet = array();
+    foreach ($originalStringAsArray as $letter){
+        if (!isset($dataSet[$letter])){
+            $dataSet[$letter] = 1;
+        }
+        else {
+            $dataSet[$letter] = $dataSet[$letter] + 1;
+        }
+    }
+    $possibleAnagramAsArray = str_split($anagramCandidate);
+    foreach ($possibleAnagramAsArray as $letter){
+        if (!isset($dataSet[$letter])){
+            return false;
+        }
+        else {
+            $dataSet[$letter] = $dataSet[$letter] - 1;
+        }
+    }
+    return true;
+}
 
 echo invert_string('This is a test');
+echo "\n";
+var_dump(is_anagram('home', 'mohe'));
+var_dump(is_anagram('test', 'home'));
